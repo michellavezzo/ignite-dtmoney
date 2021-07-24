610359b13b0258fb9c501a19c8174b0a6fa9f18d
Michel Lavezzo <michellavezzo@gmail.com>
2021-07-23 23:16:06 -0300

    global styles finished

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -37,6 +37,13 @@ export const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased; //engine chrome/(deixa fonte menos serrilhadas)
   }
 
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+  h1, h2 ,h3, h4, h5, h5, h6, strong {
+    font-weight: 600;  
+  }
 
   button {
     cursor: pointer;
