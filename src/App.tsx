b5d18666ddf73ae3b1c7a3ea4a9d32d9d6ef8364
Michel Lavezzo <michellavezzo@gmail.com>
2021-07-26 16:54:04 -0300

    Header component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Header } from "./Components/Header";
 import { GlobalStyle } from "./styles/global";
 
 const Title = styled.h1`
@@ -8,10 +9,10 @@ const Title = styled.h1`
 
 export function App() {
   return (
-    <div className="App">
-      <Title>Hello world</Title>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
 
---
+++ src/Components/Dashboard/index.tsx
@@ -0,0 +1,3 @@
+export function Dashboard() {
+  return <Containter></Containter>;
+}
