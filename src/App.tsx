6b2b2101395f47eebdc9a43ceb3ce137637455a0
Michel Lavezzo <michellavezzo@gmail.com>
2021-07-26 18:32:04 -0300

    axios implemented to fetch fake data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import styled from "styled-components";
 import { Dashboard } from "./Components/Dashboard";
 import { Header } from "./Components/Header";
 import { GlobalStyle } from "./styles/global";
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`;
 
 export function App() {
   return (
