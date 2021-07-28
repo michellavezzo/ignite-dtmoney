b1a0182ff7a7f9226039fb101ec86dcf74dc6fbd
Michel Lavezzo <michellavezzo@gmail.com>
2021-07-28 17:21:10 -0300

    modal -> create and config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
+import Modal from "react-modal";
+
 import { Dashboard } from "./Components/Dashboard";
 import { Header } from "./Components/Header";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement("root"); //questão de acessibilidade para dar o "foco" no mdal
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState<boolean>(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
