18288582ac16eb256560de44df7443c4ad439c1c
Michel Lavezzo <michellavezzo@gmail.com>
2021-08-02 18:50:42 -0300

    insertion data finalized

--- src/Components/NewTransactionModal/index.tsx
+++ src/Components/NewTransactionModal/index.tsx
@@ -24,16 +24,21 @@ export function NewTransactionModal({
   const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     //handle pattern (info set by user)
     event.preventDefault();
 
-    createTransaction({
+    await createTransaction({
       title,
       amount: amount,
       category,
       type,
     });
+    setTitle("");
+    setAmount(0);
+    setCategory("");
+    setType("deposit");
+    onRequestClose();
   }
 
   return (
